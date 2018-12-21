56938d25db40fe81df255820bc51340f97ace9d3
jyf <ji_yf@Ctrip.com>
2018-12-21 11:31:19 +0800

    commit

--- src/HeaderFixed/index.js
+++ src/HeaderFixed/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { noop, isInWeChat, isInCtripApp } from '../tools';
+import { noop, isInWeChat } from '../tools';
 import { classPrefixer } from '../config';
 
 const limitHeight = 40;
--- src/tools.js
+++ src/tools.js
@@ -7,17 +7,6 @@ const isInWeChatFn = () => {
   return /MicroMessenger/i.test(navigator.userAgent);
 };
 
-const isInCtripAppFn = () => {
-    if (!isInBrowser) {
-        return false;
-    }
-    return /(CtripWireless|sml_wireless|gs_wireless|we_wireless|TieyouWireless)/i.test(navigator.userAgent);
-};
-
-export const isInCtripApp = isInCtripAppFn();
-
-export const isInMaster = isInBrowser ? /(Ctrip|Unicom|Pro)_CtripWireless/.test(navigator.userAgent) : false;
-
 export const isInWeChat = isInWeChatFn()
 
 export const noop = () => {}
